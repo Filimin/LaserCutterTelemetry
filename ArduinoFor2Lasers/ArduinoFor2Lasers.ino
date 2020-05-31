297394fcd6e1286c4057faac1f04c9df7f2d9743
atomicwire <atomicwire@gmail.com>
2020-05-30 22:20:50 -0500

    Update ArduinoFor2Lasers.ino
    To hell with you guys. I can do what ever I want.

--- ArduinoFor2Lasers/ArduinoFor2Lasers.ino
+++ ArduinoFor2Lasers/ArduinoFor2Lasers.ino
@@ -1,3 +1,14 @@
+/*  ArduinoFor2Lasers.ino rev 5/30/2020 Tom
+* Monitor two inputs from two Laser machines Lily and Daniel at Technology for Humankind
+* One input indicates the laser is firing. The other input indicates the lid is raised
+* Output to a set of Neopixel Lights the status of the laser according to the inputs.
+* If the laser is on the set of lights for that machine will be BLUE. 
+* If the laser is off and the lid is down the set of lights for that machine will be GREEN. 
+* If the laser is on and the lid is raised the set of lights for that machine will be RED. 
+* If the Green stays on for over 1 Minute the GREEN lights will begin FLASHING. 
+* If the FLASHING continues for over 5 minutes an alarm output will sound a bell
+*/
+#define Debug 0
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
@@ -8,23 +19,34 @@
 #define NUMPIXELS      10
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
+const long LaserTimeout = 60000; // One Minute timeout interval
+const long AlarmTimeout = 300000; // Five Minute Alarm timeout interval
+int bellCount;
 int delayval = 50; // delay for half a second
-float avg0;
+int avg0 = 0;
+int avgLaser0 = 0;
+int avgLaser1 = 0;
 float avg1;
 bool isOpen0 = true;
 bool isOpen1 = true;
 bool lightOff0 = 0; 
-bool lightOff1 = 0; //------------------
-//static uint32_t lightOff1 = 0; //------------------
+bool lightOff1 = 0; 
+bool Blink0 = 0;
+bool Blink1 = 0;
+
+unsigned long endTime0 = 0;        // will store last time LED not green
+unsigned long endTime1 = 0;        // will store last time LED not green
+unsigned long endTimeBell = 0;        // will store last time LED not green
 
 void setup() {
-  pinMode(A0, INPUT_PULLUP);
-  pinMode(A1, INPUT_PULLUP);  
+  pinMode(A0, INPUT_PULLUP);  // laser power 0
+  pinMode(A1, INPUT_PULLUP);  // laser power 1
   pinMode(A2, INPUT_PULLUP);  
-  pinMode(2, INPUT_PULLUP);  
-  pinMode(3, INPUT_PULLUP);  
+  pinMode(2, INPUT_PULLUP);  // lid open 0
+  pinMode(3, INPUT_PULLUP);  // lid open 1
   pinMode(4, INPUT_PULLUP);  
   pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);    // builtin LED
   Serial.begin(9600);
   Serial.println("Hello");
   strip.begin();
@@ -32,48 +54,70 @@ void setup() {
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
   // Photocell 0 ---------------------
-  //Serial.println(analogRead(A0));
-  lightOff0 = (analogRead(A0) >= 40 ? 1 : 0);
+  //if(Debug)Serial.println(analogRead(A0));
+  //if(Debug){Serial.print(avgLaser0,DEC);Serial.print(" . ");}
+  int valLaser0 = (analogRead(A0) >= 40 ? 1000 : 0);
+  avgLaser0 = ((19*avgLaser0) + valLaser0) / 20;
+  lightOff0 = (avgLaser0 < 980);
 
   // Photocell 1 ---------------------
-  //Serial.println(analogRead(A1));
-  lightOff1 = (analogRead(A1) >= 40 ? 1 : 0);
+  //if(Debug)Serial.println(analogRead(A1));
+  if(Debug){Serial.println(avgLaser1,DEC);Serial.print(" . ");}
+  int valLaser1 = (analogRead(A1) >= 40 ? 1000 : 0);
+  avgLaser1 = ((19*avgLaser1) + valLaser1) / 20;
+  lightOff1 = (avgLaser1 < 980);
 
   //Tilt Sensor 0 --------------
-  int val0 = digitalRead(2)*100;
-//  int val0 = analogRead(A1);
-  avg0 = ((3.0*avg0)+val0)/4.0;
-
-  if (avg0 > 50) {
-    isOpen0 = false;
-  }
-  else {
-    isOpen0 = true;
-    //lightOff = 0;
-  }
+  //if(Debug){Serial.print(avg0,DEC);Serial.print(" . ");}
+  int val0 = digitalRead(2)*1000;
+  avg0 = ((9*avg0)+val0)/10;
+  isOpen0 = (avg0 > 100);
   
-  //Tilt Sensor 1 -------------
-  int val1 = digitalRead(3)*100;
-//  int val1 = analogRead(A1);
-  avg1 = ((3.0*avg1)+val1)/4.0;
+  //Tilt Sensor 1 --------------
+  //if(Debug){Serial.print(avg1,DEC);Serial.print(" . ");}
+  int val1 = digitalRead(3)*1000;
+  avg1 = ((9*avg1)+val1)/10;
+  isOpen1 = (avg1 > 100);
 
-  if (avg1 > 50) {
-    isOpen1 = false;
-  }
-  else {
-    isOpen1 = true;
-    //lightOff = 0;
-  }
-  delay(90);
-//  Serial.println(!isOpen1*8 + !lightOff1*4 + !isOpen0*2 + !lightOff0,BIN);
-  Serial.write(!isOpen1*8 + !lightOff1*4 + !isOpen0*2 + !lightOff0);
+  //Laser0 Timoout Flash -------------------
+  //if(Debug){Serial.print(lightOff0,DEC);Serial.print(" ");}
+  if(lightOff0 || isOpen0){
+    endTime0 = millis() + LaserTimeout;
+    endTimeBell = millis() + AlarmTimeout;
+    Blink0 = 0;
+  }else if(currentMillis > endTime0){Blink0 = !Blink0;}
+  
+  //Laser1 Timoout Flash -------------------
+  if(Debug){Serial.print(lightOff1,DEC);Serial.print(" ");}
+  if(lightOff1 || isOpen1){
+    endTime1 = millis() + LaserTimeout;
+    endTimeBell = millis() + AlarmTimeout;
+    Blink1 = 0;
+  }else if(currentMillis > endTime1){Blink1 = !Blink1;}
+
+  //Alarm Timeout --------------
+  if(currentMillis > endTimeBell){
+    bellCount = (bellCount + 1) % 10;
+    digitalWrite(13,HIGH);
+    digitalWrite(12,(bellCount == 2 ? 1 : 0));
+    } else {
+      digitalWrite(13,LOW);
+      digitalWrite(12,LOW);
+    }
+  
+  delay(120);  // loop delay
+  
+  //if(Debug)Serial.print(!isOpen1*8 + !lightOff1*4 + !isOpen0*2 + !lightOff0,BIN);
+  if(Debug)Serial.println(".");
+  if(!Debug)Serial.write(!isOpen1*8 + !lightOff1*4 + !isOpen0*2 + !lightOff0);
   int Brightness = 128;
-  int RED0 = !lightOff0 * Brightness;
-  int GRN0 = !lightOff0 * !isOpen0 * Brightness;
+  int RED0 = !lightOff0 * isOpen0 * Brightness;
+  int GRN0 = !lightOff0 * !isOpen0 * !Blink0 * Brightness;
   int BLU0 = lightOff0 * Brightness;
-  int RED1 = !lightOff1 * Brightness;
-  int GRN1 = !lightOff1 * !isOpen1 * Brightness;
+  int RED1 = !lightOff1 * isOpen1 * Brightness;
+  int GRN1 = !lightOff1 * !isOpen1 * !Blink1 * Brightness;
   int BLU1 = lightOff1 * Brightness;
   colorWipe(strip.Color(RED0, GRN0, BLU0),strip.Color(RED1, GRN1, BLU1), 30);
 }
@@ -87,5 +131,3 @@ void colorWipe(uint32_t c,uint32_t d, uint8_t wait) {
   delay(wait);
   strip.show();
  }
-
-
